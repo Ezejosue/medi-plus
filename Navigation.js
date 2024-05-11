50478f91f2381c92fe22b522f376bc170a1769da
Josué Ávalos <aezequiel56@gmail.com>
2024-05-10 18:42:04 -0600

    Corrections

--- Navigation.js
+++ Navigation.js
@@ -11,74 +11,74 @@ import SearchMedicalRecordScreen from "./pages/SearchMedicalRecordScreen";
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
-    const [userInfo, setUserInfo] = useState(null);
-    const [navigationState, setNavigationState] = useState();
+  const [userInfo, setUserInfo] = useState(null);
+  const [navigationState, setNavigationState] = useState();
 
-    useEffect(() => {
-        const checkLoginStatus = async () => {
-            const user = await AsyncStorage.getItem("@user");
-            if (user) {
-                setUserInfo(JSON.parse(user));
-            }
-        };
-        checkLoginStatus();
-        const loadNavigationState = async () => {
-            const savedState = await AsyncStorage.getItem("@navigationState");
-            if (savedState) {
-                setNavigationState(JSON.parse(savedState));
-            }
-        };
-        loadNavigationState();
-    }, []);
-
-    const handleLogin = (user) => {
-        setUserInfo(user);
-        AsyncStorage.setItem("@user", JSON.stringify(user));
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const user = await AsyncStorage.getItem("@user");
+      if (user) {
+        setUserInfo(JSON.parse(user));
+      }
     };
-
-    const handleLogout = () => {
-        AsyncStorage.removeItem("@user");
-        setUserInfo(null);
+    checkLoginStatus();
+    const loadNavigationState = async () => {
+      const savedState = await AsyncStorage.getItem("@navigationState");
+      if (savedState) {
+        setNavigationState(JSON.parse(savedState));
+      }
     };
-    return (
-        <NavigationContainer
-            initialState={navigationState}
-            onStateChange={(state) =>
-                AsyncStorage.setItem("@navigationState", JSON.stringify(state))
-            }
-        >
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                {userInfo ? (
-                    <>
-                        <Stack.Screen name="Home">
-                            {(props) => (
-                                <HomeScreen
-                                    {...props}
-                                    userInfo={userInfo}
-                                    onLogout={handleLogout}
-                                />
-                            )}
-                        </Stack.Screen>
-                        <Stack.Screen
-                            name="MedicalRecord"
-                            component={MedicalRecordScreen}
-                        />
-                        <Stack.Screen
-                            name="SearchMedicalRecord"
-                            component={SearchMedicalRecordScreen}
-                        />
-                    </>
-                ) : (
-                    <>
-                        <Stack.Screen name="Login">
-                            {(props) => <LoginScreen {...props} onLogin={handleLogin} />}
-                        </Stack.Screen>
-                        <Stack.Screen name="Register" component={RegisterScreen} />
-                    </>
-                )}
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+    loadNavigationState();
+  }, []);
+
+  const handleLogin = (user) => {
+    setUserInfo(user);
+    AsyncStorage.setItem("@user", JSON.stringify(user));
+  };
+
+  const handleLogout = () => {
+    AsyncStorage.removeItem("@user");
+    setUserInfo(null);
+  };
+  return (
+    <NavigationContainer
+      initialState={navigationState}
+      onStateChange={(state) =>
+        AsyncStorage.setItem("@navigationState", JSON.stringify(state))
+      }
+    >
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {userInfo ? (
+          <>
+            <Stack.Screen name="Home">
+              {(props) => (
+                <HomeScreen
+                  {...props}
+                  userInfo={userInfo}
+                  onLogout={handleLogout}
+                />
+              )}
+            </Stack.Screen>
+            <Stack.Screen
+              name="MedicalRecord"
+              component={MedicalRecordScreen}
+            />
+            <Stack.Screen
+              name="SearchMedicalRecord"
+              component={SearchMedicalRecordScreen}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Login">
+              {(props) => <LoginScreen {...props} onLogin={handleLogin} />}
+            </Stack.Screen>
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Navigation;
